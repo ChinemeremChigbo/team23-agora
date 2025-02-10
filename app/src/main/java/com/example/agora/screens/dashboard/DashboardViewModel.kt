12de4f72ef35bf0da4af37b153f4b4b52b053b07
Justin Freemantle <jdkfree@gmail.com>
2025-02-10 00:17:36 -0500

    other changes..

--- app/src/main/java/com/example/agora/screens/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/agora/screens/dashboard/DashboardViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.agora.ui.home
+package com.example.agora.screens.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
