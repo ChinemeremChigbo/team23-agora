3ccd219965bd3129945db736f4a0c49ac8e6fbc0
Chinemerem <chinemeremchigbo@Outlook.com>
2025-02-11 16:15:02 -0500

    Remove unused imports

--- app/src/main/java/com/example/agora/AuthActivity.kt
+++ app/src/main/java/com/example/agora/AuthActivity.kt
@@ -3,10 +3,6 @@ package com.example.agora
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import com.example.agora.screens.authentication.sign_in.SignInScreen
 
 class AuthActivity : ComponentActivity() {
