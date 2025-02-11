37bb0486dd863513df5b3ac556f541446f399fe6
Samantha-Zhan <samanthazhanjiaqi@gmail.com>
2025-02-11 08:39:59 -0500

    convert nav bar/screens to use jetpack compose

---
+++ app/src/main/java/com/example/agora/screens/MainScreen.kt
@@ -0,0 +1,46 @@
+package com.example.agora.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.agora.screens.explore.ExploreScreen
+import com.example.agora.screens.notifications.NotificationScreen
+import com.example.agora.screens.post.PostScreen
+import com.example.agora.screens.settings.SettingScreen
+import com.example.agora.screens.wishlist.WishlistScreen
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(navController)
+        },
+    ) {
+            innerPadding ->
+        NavigationHost(navController, Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun NavigationHost(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomNavItem.Explore.route,
+        modifier = modifier
+    ) {
+        composable(BottomNavItem.Explore.route) { ExploreScreen() }
+        composable(BottomNavItem.Post.route) { PostScreen() }
+        composable(BottomNavItem.Wishlist.route) { WishlistScreen() }
+        composable(BottomNavItem.Inbox.route) { NotificationScreen() }
+        composable(BottomNavItem.Setting.route) { SettingScreen() }
+    }
+}
