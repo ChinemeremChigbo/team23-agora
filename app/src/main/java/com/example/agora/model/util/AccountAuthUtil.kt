38b0d018a2910e4c1c706eefbaf47f06d4662485
Samantha-Zhan <samanthazhanjiaqi@gmail.com>
2025-02-23 19:32:50 -0500

    add signin state management + improve signin logic

--- app/src/main/java/com/example/agora/model/util/AccountAuthUtil.kt
+++ app/src/main/java/com/example/agora/model/util/AccountAuthUtil.kt
@@ -5,19 +5,22 @@ import kotlinx.coroutines.tasks.await
 
 class AccountAuthUtil {
     companion object {
-        private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
-        suspend fun accountSignIn(email: String, password: String) {
-            auth.signInWithEmailAndPassword(email, password).await()
+        suspend fun accountSignIn(auth: FirebaseAuth, email: String, password: String) {
+            val result = auth.signInWithEmailAndPassword(email, password).await()
+            val user = result.user
+            if (user != null && !user.isEmailVerified) {
+                auth.signOut()
+                throw Exception("Please verify your email before logging in!")
+            }
             // TODO: create currentUser auth listener which should grab the login info (i.e. uuid)
         }
-        suspend fun accountSignUp(email: String, password: String) {
+        suspend fun accountSignUp(auth: FirebaseAuth, email: String, password: String) {
             auth.createUserWithEmailAndPassword(email, password).await()
         }
-        suspend fun signOut() {
+        fun signOut(auth: FirebaseAuth) {
             auth.signOut()
         }
-        suspend fun deleteAccount() {
+        suspend fun deleteAccount(auth: FirebaseAuth) {
             auth.currentUser!!.delete().await()
         }
     }
