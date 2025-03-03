82271f7b7fd4fe1ef802b69bf45274f078cd5eb5
jenniferyu12 <77082395+jenniferyu12@users.noreply.github.com>
2025-03-03 03:14:00 -0500

    user detail modal

--- app/src/main/java/com/example/agora/screens/postDetail/PostDetailViewModel.kt
+++ app/src/main/java/com/example/agora/screens/postDetail/PostDetailViewModel.kt
@@ -3,6 +3,7 @@ package com.example.agora.screens.postDetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.agora.model.data.Post
+import com.example.agora.model.data.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -13,6 +14,9 @@ class PostDetailViewModel (
     private val _post = MutableStateFlow<Post?>(null)
     val post = _post.asStateFlow()
 
+    private val _user = MutableStateFlow<User?>(null)
+    val user = _user.asStateFlow()
+
     init {
         fetchPostDetails(postId)
     }
@@ -20,6 +24,12 @@ class PostDetailViewModel (
         // TODO (for eddie, from jennifer) - query the post based off postId
         // will also need a separate query for the user details to display in the user section
         _post.value = Post(postId = postId, title = "hehe")
+        _user.value = User(
+            fullName = "test user",
+            bio = "Hi! I am a student at the University of Waterloo",
+            email = "test@test.com",
+            profileImage = "https://picsum.photos/200"
+        )
     }
 }
 
