8379c4d63398ca523e1c32419367c4d72680d906
Samantha <40647818+Samantha-Zhan@users.noreply.github.com>
2025-03-02 19:26:03 -0500

    [Backend] Implement User Registration API (#69)
    * [Backend] Implement User Registration API
    
    * add missing fields
    
    * refactor
    
    * remove createUser + add phone number

--- app/src/main/java/com/example/agora/model/data/Comment.kt
+++ app/src/main/java/com/example/agora/model/data/Comment.kt
@@ -4,24 +4,24 @@ import java.util.*
 import java.sql.Timestamp
 
 class Comment(
-    private val commentId: UUID = UUID.randomUUID(),
-    private val creatorId: UUID = UUID.randomUUID(),
+    private val commentId: String = "",
+    private val creatorId: String = "",
     private var text: String = "",
-    private var mentions: Array<UUID> = arrayOf(),
+    private var mentions: Array<String> = arrayOf(),
     private var createdAt: Timestamp = Timestamp(System.currentTimeMillis())
 ) {
 
     // Getters and Setters
 
     // comment and creator id should not be changeable
-    fun getCommentId(): UUID = commentId
-    fun getCreatorId(): UUID = creatorId
+    fun getCommentId(): String = commentId
+    fun getCreatorId(): String = creatorId
 
     fun getText(): String = text
     fun setText(value: String) { text = value }
 
-    fun getMentions(): Array<UUID> = mentions
-    fun setMentions(value: Array<UUID>) { mentions = value }
+    fun getMentions(): Array<String> = mentions
+    fun setMentions(value: Array<String>) { mentions = value }
 
     fun getCreatedAt(): Timestamp = createdAt
     fun setCreatedAt(value: Timestamp) { createdAt = value }
--- app/src/main/java/com/example/agora/model/data/User.kt
+++ app/src/main/java/com/example/agora/model/data/User.kt
@@ -1,44 +1,78 @@
 package com.example.agora.model.data
-import java.util.*
+import com.google.firebase.firestore.FirebaseFirestore
+import java.sql.Timestamp
 
 enum class UserStatus {
     ACTIVATED, DEACTIVATED
 }
 
 class User(
-    private var userId: UUID = UUID.randomUUID(),
-    private var status: UserStatus = UserStatus.ACTIVATED,
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance(),
+    var userId: String = "",
+    var status: UserStatus = UserStatus.ACTIVATED,
     var username: String = "",
-    var firstName: String = "",
-    var lastName: String = "",
+    var fullName: String = "",
     var bio: String = "",
     var profileImage: String = "",
     var email: String = "",
-    var phoneNumber: String = ""
+    var phoneNumber: String = "",
+    var country: String = "",
+    var state: String = "",
+    var city: String = "",
+    var postalCode: String = "",
+    var address: String = "",
+    var wishList: MutableMap<String, Timestamp> = mutableMapOf()
 ) {
-
-    // Getters and Setters
-    fun getUserId(): UUID = userId
-    fun setUserId(value: UUID) { userId = value }
-
-    fun getStatus(): UserStatus = status
-    fun setStatus(value: UserStatus) { status = value }
-
     // Methods
-    fun login(password: String): Boolean {
-        // TODO
-        return false
-    }
-
+    // NOTE: we don't need login method as Firebase handle password management
     fun register() {
-        // TODO
+        // Use the userId as the document ID
+        db.collection("users").document(userId)
+            .set(mapOf(
+                "status" to status.name,  // Convert Enum to String
+                "username" to username,
+                "fullName" to fullName,
+                "bio" to bio,
+                "profileImage" to profileImage,
+                "email" to email,
+                "phoneNumber" to phoneNumber,
+                "address" to mapOf(
+                    "country" to country,
+                    "city" to city,
+                    "state" to state,
+                    "address" to address,
+                    "postalCode" to postalCode
+                ),
+                "wishlist" to wishList,
+            ))
+            .addOnSuccessListener {
+                println("User successfully added to Firestore!")
+            }
+            .addOnFailureListener { e ->
+                println("Error adding user: ${e.message}")
+            }
     }
 
+    /**
+     * sample uage:
+     * val updates = mapOf(
+     *     "bio" to "Updated bio",
+     *     "phoneNumber" to "9876543210"
+     * )
+     */
     fun updateInfo(newInfo: Map<String, Any>) {
-        // TODO
+        db.collection("users").document(userId)
+            .update(newInfo)
+            .addOnSuccessListener {
+                println("User information updated successfully!")
+            }
+            .addOnFailureListener { e ->
+                println("Error updating user: ${e.message}")
+            }
     }
 
-    fun changeStatus(newStatus: UserStatus) {
+    fun updateUserStatus(newStatus: UserStatus) {
         status = newStatus
+        updateInfo(mapOf("status" to newStatus.name))
     }
 }
