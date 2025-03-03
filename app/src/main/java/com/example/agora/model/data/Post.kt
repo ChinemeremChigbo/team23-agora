d613f3aa3e129577394f3bb0eb40167e441ee802
Justin Freemantle <50429451+jdkfree@users.noreply.github.com>
2025-03-03 13:42:05 -0500

    [backend] refactor post utils (#75)
    * move post utils to repository layer
    
    * put utils in companion object

--- app/src/main/java/com/example/agora/model/data/Post.kt
+++ app/src/main/java/com/example/agora/model/data/Post.kt
@@ -39,54 +39,6 @@ class Post(
     fun getCreatedAt(): Timestamp = createdAt
     fun setCreatedAt(value: Timestamp) { createdAt = value }
 
-    fun filterPosts(
-        minPrice: Int? = null,
-        maxPrice: Int? = null,
-        category: Category? = null,
-        sortByPrice: Boolean = false,
-        priceLowToHi: Boolean = true,
-        callback: (List<Map<String, Any>>) -> Unit
-    ) {
-        val db = FirebaseFirestore.getInstance()
-
-        var query: Query = db.collection("posts")
-
-        minPrice?.let {
-            query = query.whereGreaterThanOrEqualTo("price", minPrice)
-        }
-
-        maxPrice?.let {
-            query = query.whereLessThanOrEqualTo("price", maxPrice)
-        }
-
-        category?.let {
-            query = query.whereEqualTo("category", category.name)
-        }
-
-        query = if (sortByPrice) {
-            query.orderBy(
-                "price", if (priceLowToHi) Query.Direction.ASCENDING else Query.Direction.DESCENDING
-            )
-        } else {
-            query.orderBy(
-                "createdAt", Query.Direction.DESCENDING
-            )
-        }
-
-        query.get()
-            .addOnSuccessListener { documents ->
-                val resultList = mutableListOf<Map<String, Any>>()
-                for (document in documents) {
-                    resultList.add(document.data)
-                }
-                callback(resultList)
-            }
-            .addOnFailureListener { exception ->
-                println("Error getting posts: $exception")
-                callback(emptyList())
-            }
-    }
-
     fun updateInfo(newInfo: Map<String, Any>) {
         // TODO
     }
