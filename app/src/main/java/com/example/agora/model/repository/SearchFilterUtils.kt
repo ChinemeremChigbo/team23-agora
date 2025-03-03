d11184ad7a1aaf10a1b28152cbba9c6272adaa50
Justin Freemantle <jdkfree@gmail.com>
2025-03-03 13:37:47 -0500

    put utils in companion object

--- app/src/main/java/com/example/agora/model/repository/SearchFilterUtils.kt
+++ app/src/main/java/com/example/agora/model/repository/SearchFilterUtils.kt
@@ -5,51 +5,54 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
 class SearchFilterUtils {
-    fun filterPosts(
-        minPrice: Int? = null,
-        maxPrice: Int? = null,
-        category: Category? = null,
-        sortByPrice: Boolean = false,
-        priceLowToHi: Boolean = true,
-        callback: (List<Map<String, Any>>) -> Unit
-    ) {
-        val db = FirebaseFirestore.getInstance()
+    companion object {
+        fun filterPosts(
+            minPrice: Int? = null,
+            maxPrice: Int? = null,
+            category: Category? = null,
+            sortByPrice: Boolean = false,
+            priceLowToHi: Boolean = true,
+            callback: (List<Map<String, Any>>) -> Unit
+        ) {
+            val db = FirebaseFirestore.getInstance()
 
-        var query: Query = db.collection("posts")
+            var query: Query = db.collection("posts")
 
-        minPrice?.let {
-            query = query.whereGreaterThanOrEqualTo("price", minPrice)
-        }
+            minPrice?.let {
+                query = query.whereGreaterThanOrEqualTo("price", minPrice)
+            }
 
-        maxPrice?.let {
-            query = query.whereLessThanOrEqualTo("price", maxPrice)
-        }
+            maxPrice?.let {
+                query = query.whereLessThanOrEqualTo("price", maxPrice)
+            }
 
-        category?.let {
-            query = query.whereEqualTo("category", category.name)
-        }
+            category?.let {
+                query = query.whereEqualTo("category", category.name)
+            }
 
-        query = if (sortByPrice) {
-            query.orderBy(
-                "price", if (priceLowToHi) Query.Direction.ASCENDING else Query.Direction.DESCENDING
-            )
-        } else {
-            query.orderBy(
-                "createdAt", Query.Direction.DESCENDING
-            )
-        }
+            query = if (sortByPrice) {
+                query.orderBy(
+                    "price",
+                    if (priceLowToHi) Query.Direction.ASCENDING else Query.Direction.DESCENDING
+                )
+            } else {
+                query.orderBy(
+                    "createdAt", Query.Direction.DESCENDING
+                )
+            }
 
-        query.get()
-            .addOnSuccessListener { documents ->
-                val resultList = mutableListOf<Map<String, Any>>()
-                for (document in documents) {
-                    resultList.add(document.data)
+            query.get()
+                .addOnSuccessListener { documents ->
+                    val resultList = mutableListOf<Map<String, Any>>()
+                    for (document in documents) {
+                        resultList.add(document.data)
+                    }
+                    callback(resultList)
                 }
-                callback(resultList)
-            }
-            .addOnFailureListener { exception ->
-                println("Error getting posts: $exception")
-                callback(emptyList())
-            }
+                .addOnFailureListener { exception ->
+                    println("Error getting posts: $exception")
+                    callback(emptyList())
+                }
+        }
     }
 }
