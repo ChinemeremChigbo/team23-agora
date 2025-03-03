f8597645e8fd42fa6bb5eaadd374c13352ec7ef2
Justin Freemantle <jdkfree@gmail.com>
2025-03-03 13:56:10 -0500

    add getters and setters to Wishlist.kt

--- app/src/main/java/com/example/agora/model/data/Wishlist.kt
+++ app/src/main/java/com/example/agora/model/data/Wishlist.kt
@@ -1,10 +1,24 @@
 package com.example.agora.model.data
 
-import com.google.firebase.firestore.FirebaseFirestore
-import java.util.*
 import java.sql.Timestamp
 
-class WishList {
-    private val userId: String = ""
+class WishList(private var userId: String = "") {
+
     private val posts: MutableMap<String, Timestamp> = mutableMapOf()
+
+    fun getUserId(): String {
+        return userId
+    }
+
+    fun getPosts(): Map<String, Timestamp> {
+        return posts.toMap()
+    }
+
+    fun addPost(postId: String, timestamp: Timestamp) {
+        posts[postId] = timestamp
+    }
+
+    fun removePost(postId: String) {
+        posts.remove(postId)
+    }
 }
