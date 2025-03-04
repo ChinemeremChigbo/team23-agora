c8604f83cb7fda09278f8a9c16175b3132dbcdcd
jenniferyu12 <77082395+jenniferyu12@users.noreply.github.com>
2025-03-04 01:32:30 -0500

    basic set up

---
+++ app/src/main/java/com/example/agora/screens/search/SearchViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.agora.screens.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.agora.model.data.Post
+import com.example.agora.model.repository.SearchFilterUtils
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SearchViewModel(initialSearchText: String = ""): ViewModel() {
+
+    private val _posts = MutableStateFlow<List<Post>>(emptyList())
+    val posts = _posts.asStateFlow()
+
+    private val _searchText = MutableStateFlow<String>(initialSearchText)
+    val searchText = _searchText.asStateFlow()
+
+    private val _isExpanded = MutableStateFlow(false)
+    val isExpanded = _isExpanded.asStateFlow()
+
+    private val _recentSearches = MutableStateFlow<List<String>>(listOf(initialSearchText))
+    val recentSearches = _recentSearches.asStateFlow()
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+
+    fun onExpandedChange(expanded: Boolean) {
+        _isExpanded.value = expanded
+    }
+
+    fun onSearchSubmitted(query: String) {
+        if (query.isNotBlank() && !_recentSearches.value.contains(query)) {
+            _recentSearches.value =
+                listOf(query) + _recentSearches.value.take(4) // Store up to 5 recent searches
+        }
+        _searchText.value = query
+        _isExpanded.value = false
+    }
+
+    init {
+        fetchResults()
+    }
+
+    fun fetchResults() {
+        SearchFilterUtils.getPosts() { posts ->
+            _posts.value = posts.map { post -> Post.convertDBEntryToPostDetail(post)}
+        }
+    }
+}
+
+class SearchViewModelFactory(private val searchText: String) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return SearchViewModel(searchText) as T
+    }
+}
