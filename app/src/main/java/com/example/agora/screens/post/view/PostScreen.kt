79f339361cd085fdb8ffffe8595ce5f1ab9b6a5d
edwardna1 <77164264+edwardna1@users.noreply.github.com>
2025-03-04 02:59:09 -0500

    moved preview screen

--- app/src/main/java/com/example/agora/screens/post/view/PostScreen.kt
+++ app/src/main/java/com/example/agora/screens/post/view/PostScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun PostScreen(navController: NavController) {
@@ -30,4 +32,12 @@ fun PostScreen(navController: NavController) {
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun PostPreview() {
+    val navController = rememberNavController()
+    PostScreen(navController = navController)
+}
+
 
