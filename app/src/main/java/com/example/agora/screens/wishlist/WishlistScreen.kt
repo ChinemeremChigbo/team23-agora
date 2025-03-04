b4802905021dcc9bffc219807dae83e75782df92
jenniferyu12 <77082395+jenniferyu12@users.noreply.github.com>
2025-03-04 01:20:02 -0500

    wishlist ui YAY! (#91)
    * basic
    
    * most of grid
    
    * wip
    
    * clean up
    
    * cleaner hehe haha
    
    * refetch wishlist

--- app/src/main/java/com/example/agora/screens/wishlist/WishlistScreen.kt
+++ app/src/main/java/com/example/agora/screens/wishlist/WishlistScreen.kt
@@ -1,34 +1,44 @@
 package com.example.agora.screens.wishlist
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.agora.ui.components.BasicPostGrid
 
 @Composable
-fun WishlistScreen() {
+fun WishlistScreen(viewModel: WishlistViewModel = viewModel(), navController: NavController) {
+    val posts by viewModel.posts.collectAsState()
+
+    LaunchedEffect(navController.currentBackStackEntry) {
+        viewModel.fetchWishlist()
+    }
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(top=21.dp, bottom=0.dp, start=21.dp, end=21.dp).fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = "Wishlist Screen",
-            fontSize = 40.sp,
-            textAlign = TextAlign.Center
+            text = "Wishlist",
+            fontSize = 19.sp,
+            fontWeight = FontWeight.Bold
         )
+
+        Spacer(Modifier.size(40.dp))
+
+        BasicPostGrid(posts, navController)
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun WishlistPreview() {
-    WishlistScreen()
-}
-
