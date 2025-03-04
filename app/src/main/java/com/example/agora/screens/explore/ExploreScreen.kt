afaaed202834fd9394b4c707360bebea9ce9cfc7
edwardna1 <77164264+edwardna1@users.noreply.github.com>
2025-03-03 19:48:55 -0500

    change getter from getPostId() to post.postid

--- app/src/main/java/com/example/agora/screens/explore/ExploreScreen.kt
+++ app/src/main/java/com/example/agora/screens/explore/ExploreScreen.kt
@@ -109,7 +109,7 @@ fun ExploreScreen(viewModel: ExploreViewModel = viewModel(), navController: NavC
                     Text(title, fontSize = 19.sp, fontWeight = FontWeight.Bold)
                     LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
                         items(section) { post ->
-                            PostPreview(post, onClick = {navController.navigate("post_detail/${post.getPostId()}")})
+                            PostPreview(post, onClick = {navController.navigate("post_detail/${post.postId}")})
                         }
                     }
                 }
