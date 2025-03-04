d38b290f922027b84b0670ff064d887ed1962b6d
Edward Na <edwardna91@gmail.com>
2025-03-04 14:06:25 -0500

    idk how this happened

--- app/src/main/java/com/example/agora/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/agora/screens/settings/SettingsViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class   SettingsViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "Settings"
