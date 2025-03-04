1b52e4405395a4edcb6b4307be11febb8afcd83d
edwardna1 <77164264+edwardna1@users.noreply.github.com>
2025-03-04 02:54:20 -0500

    changed postUtils to repository file

--- app/src/main/java/com/example/agora/screens/post/viewmodel/CreatePostViewModel.kt
+++ app/src/main/java/com/example/agora/screens/post/viewmodel/CreatePostViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.agora.model.data.Category
-import com.example.agora.model.data.PostUtils
+import com.example.agora.model.repository.PostUtils
 
 class CreatePostViewModel: ViewModel() {
 
