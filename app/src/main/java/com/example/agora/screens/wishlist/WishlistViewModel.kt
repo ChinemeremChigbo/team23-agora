a1850fc294fbe826da09961cfb952d21c76ec441
jenniferyu12 <77082395+jenniferyu12@users.noreply.github.com>
2025-03-03 19:50:00 -0500

    most of grid

--- app/src/main/java/com/example/agora/screens/wishlist/WishlistViewModel.kt
+++ app/src/main/java/com/example/agora/screens/wishlist/WishlistViewModel.kt
@@ -2,11 +2,26 @@ package com.example.agora.screens.wishlist
 
 import androidx.lifecycle.ViewModel
 import com.example.agora.model.data.Post
+import com.example.agora.model.repository.WishlistUtils
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class WishlistViewModel : ViewModel() {
+    val currentUser = FirebaseAuth.getInstance().currentUser
 
-    private val _posts = MutableStateFlow<List<Post>>(emptyList())
+    private val _posts = MutableStateFlow<List<Post>>(listOf(
+        Post(title="Fridge", price=10.12),
+        Post(title="Fridge 2", price=100.12),
+        Post(title="Fridge 3", price=121.00)
+    ))
     val posts = _posts.asStateFlow()
+
+    init {
+        currentUser?.uid?.let {
+            WishlistUtils.getWishList(currentUser.uid) { posts ->
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/agora/ui/components/BasicPostGrid.kt
+++ app/src/main/java/com/example/agora/ui/components/BasicPostGrid.kt
@@ -1,10 +1,26 @@
 package com.example.agora.ui.components
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.agora.model.data.Post
 
 @Composable
-fun BasicPostGrid(posts: List<Post>) {
-    Column {  }
+fun BasicPostGrid(posts: List<Post>, navController: NavController) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2), // 2 columns
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(posts) { post ->
+            PostPreview(post, { navController.navigate("post_detail/${post.getPostId()}") })
+        }
+    }
 }
