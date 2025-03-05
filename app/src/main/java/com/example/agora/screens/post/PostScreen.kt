8a7564bf0d9745f3173f585d6d0e3bfc1a756f3c
Cindy Wang <cinwan23@gmail.com>
2025-03-05 01:20:29 -0500

    refactor to pass component

--- app/src/main/java/com/example/agora/screens/post/PostScreen.kt
+++ app/src/main/java/com/example/agora/screens/post/PostScreen.kt
@@ -1,6 +1,10 @@
 package com.example.agora.screens.post
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
