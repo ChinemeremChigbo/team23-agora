5da3ed2c6f8c915d1a110432bf31ddad0f47a612
Samantha <40647818+Samantha-Zhan@users.noreply.github.com>
2025-03-05 09:46:29 -0500

    fix navigation problem (#105)
    * fix navigation problem
    
    * fix nit

--- app/src/main/java/com/example/agora/screens/wishlist/WishlistScreen.kt
+++ app/src/main/java/com/example/agora/screens/wishlist/WishlistScreen.kt
@@ -17,28 +17,53 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.agora.screens.postDetail.PostDetailScreen
+import com.example.agora.screens.postDetail.PostDetailViewModel
+import com.example.agora.screens.postDetail.PostDetailViewModelFactory
 import com.example.agora.ui.components.BasicPostGrid
 
 @Composable
-fun WishlistScreen(viewModel: WishlistViewModel = viewModel(), navController: NavController) {
+fun WishlistScreen(viewModel: WishlistViewModel = viewModel(), parentNavController: NavController) {
     val posts by viewModel.posts.collectAsState()
+    val nestedNavController = rememberNavController()
 
-    LaunchedEffect(navController.currentBackStackEntry) {
-        viewModel.fetchWishlist()
-    }
-
-    Column(
-        modifier = Modifier.padding(top=21.dp, bottom=0.dp, start=21.dp, end=21.dp).fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    NavHost(
+        navController = nestedNavController,
+        startDestination = "wishlist"
     ) {
-        Text(
-            text = "Wishlist",
-            fontSize = 19.sp,
-            fontWeight = FontWeight.Bold
-        )
+        // Post wishlist Screen
+        composable("wishlist") {
+            LaunchedEffect(nestedNavController.currentBackStackEntry) {
+                viewModel.fetchWishlist()
+            }
 
-        Spacer(Modifier.size(40.dp))
+            Column(
+                modifier = Modifier.padding(top=21.dp, bottom=0.dp, start=21.dp, end=21.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Wishlist",
+                    fontSize = 19.sp,
+                    fontWeight = FontWeight.Bold
+                )
 
-        BasicPostGrid(posts, navController)
+                Spacer(Modifier.size(40.dp))
+
+                BasicPostGrid(posts, nestedNavController)
+            }
+        }
+        // Post Detail Screen
+        composable(
+            route = "post_detail/{postId}",
+        ) { backStackEntry ->
+            val postId = backStackEntry.arguments?.getString("postId") ?: "Unknown"
+            val postDetailViewModel: PostDetailViewModel = viewModel(factory = PostDetailViewModelFactory(postId))
+            PostDetailScreen(postDetailViewModel, nestedNavController)
+        }
+
     }
+
 }
