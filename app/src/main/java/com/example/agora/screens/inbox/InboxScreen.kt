195835e845b17738a2847af5e427ddc98f66275a
Samantha-Zhan <samanthazhanjiaqi@gmail.com>
2025-03-13 15:12:31 -0400

    map view

--- app/src/main/java/com/example/agora/screens/inbox/InboxScreen.kt
+++ app/src/main/java/com/example/agora/screens/inbox/InboxScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.agora.ui.components.MapScreen
 
 @Composable
 fun InboxScreen() {
