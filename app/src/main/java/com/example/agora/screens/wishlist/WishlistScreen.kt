913ed732890535dab9808be912a7c211e1853577
jenniferyu12 <77082395+jenniferyu12@users.noreply.github.com>
2025-03-17 20:59:31 -0400

    wishlist empty state

--- app/src/main/java/com/example/agora/screens/wishlist/WishlistScreen.kt
+++ app/src/main/java/com/example/agora/screens/wishlist/WishlistScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -22,10 +25,12 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.agora.screens.BottomNavItem
 import com.example.agora.screens.postDetail.PostDetailScreen
 import com.example.agora.screens.postDetail.PostDetailViewModel
 import com.example.agora.screens.postDetail.PostDetailViewModelFactory
 import com.example.agora.ui.components.BasicPostGrid
+import com.example.agora.ui.components.EmptyState
 
 @Composable
 fun WishlistScreen(viewModel: WishlistViewModel = viewModel(), parentNavController: NavController) {
@@ -59,7 +64,21 @@ fun WishlistScreen(viewModel: WishlistViewModel = viewModel(), parentNavControll
 
                 Spacer(Modifier.size(40.dp))
 
-                BasicPostGrid(posts, nestedNavController)
+                if (posts.isEmpty()) {
+                    EmptyState(
+                        title = "Your wishlist is empty",
+                        msg = "Like a post to add it to your wishlist",
+                        icon = Icons.Default.Image
+                    ) {
+                        Button(
+                            onClick = { parentNavController.navigate(BottomNavItem.Explore.route) }
+                        ) {
+                            Text(text = "Explore posts")
+                        }
+                    }
+                } else {
+                    BasicPostGrid(posts, nestedNavController)
+                }
             }
         }
         // Post Detail Screen
