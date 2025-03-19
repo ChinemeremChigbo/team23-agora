867c99b11b105b0da65ce4d696c4134aa280d7ec
Justin Freemantle <50429451+jdkfree@users.noreply.github.com>
2025-03-19 02:28:45 -0400

    Comments API (#139)
    * create and delete comments
    
    * get comment api and stuff
    
    * get comments for a post
    
    * remove old fn
    
    * fix finding usernames in comments
    
    * poo
    
    ---------
    
    Co-authored-by: Cindy Wang <cinwan23@gmail.com>
    Co-authored-by: jenniferyu12 <77082395+jenniferyu12@users.noreply.github.com>

--- app/src/main/java/com/example/agora/model/data/Comment.kt
+++ app/src/main/java/com/example/agora/model/data/Comment.kt
@@ -4,38 +4,23 @@ import java.util.*
 import com.google.firebase.Timestamp
 
 class Comment(
-    private val commentId: String = "",
-    private val creatorId: String = "",
-    private var text: String = "",
-    private var mentions: Array<String> = arrayOf(),
-    private var createdAt: Timestamp = Timestamp.now()
+    val commentId: String = "",
+    val userId: String = "",
+    val postId: String = "",
+    var text: String = "",
+    var createdAt: Timestamp? = Timestamp.now(),
+    var mentions: List<String> = listOf(),
 ) {
-
-    // Getters and Setters
-
-    // comment and creator id should not be changeable
-    fun getCommentId(): String = commentId
-    fun getCreatorId(): String = creatorId
-
-    fun getText(): String = text
-    fun setText(value: String) { text = value }
-
-    fun getMentions(): Array<String> = mentions
-    fun setMentions(value: Array<String>) { mentions = value }
-
-    fun getCreatedAt(): Timestamp = createdAt
-    fun setCreatedAt(value: Timestamp) { createdAt = value }
-
-    // returns a list of usernames
-    fun findMentions(): List<String> {
-        return "@(\\S+)".toRegex().findAll(text).map { it.groupValues[1] }.toList()
-    }
-
-    fun notify(users: List<String>, comment: String) {
-        // TODO
-    }
-
-    fun getFormattedComment(): String {
-        return getText()
+    companion object {
+        fun convertDBEntryToComment(entry: Map<String, Any>): Comment {
+            return Comment(
+                commentId = entry["commentId"] as? String ?: "",
+                userId = entry["userId"] as? String ?: "",
+                postId = entry["postId"] as? String ?: "",
+                text = entry["text"] as? String ?: "",
+                createdAt = entry["createdAt"] as? Timestamp,
+                mentions = entry["mentions"] as? List<String> ?: listOf()
+            )
+        }
     }
 }
