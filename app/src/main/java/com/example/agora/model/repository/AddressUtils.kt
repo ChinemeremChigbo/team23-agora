20deb8a1ad7fef5fe6931159d5ea23b61ad31485
Samantha <40647818+Samantha-Zhan@users.noreply.github.com>
2025-03-31 20:33:33 -0400

    Adding more suitable error messages for firebase auth + google map (#174)
    * firebase auth - register/signup errors
    
    * resolve merge conflit
    
    * fix klint

--- app/src/main/java/com/example/agora/model/repository/AddressUtils.kt
+++ app/src/main/java/com/example/agora/model/repository/AddressUtils.kt
@@ -1,22 +1,13 @@
 package com.example.agora.model.repository
 
-import android.util.Log
 import com.example.agora.model.util.GeocodingClient
 import com.example.agora.model.util.GeocodingService
 
 class AddressUtils {
     companion object {
         suspend fun getGeocoding(address: String): GeocodingService.Result? {
-            return try {
-                val response = GeocodingClient.instance.getGeocoding(
-                    address,
-                    GeocodingClient.API_KEY
-                )
-                response.results.firstOrNull()
-            } catch (e: Exception) {
-                Log.e("Geocoding", "Error: ${e.message}")
-                null
-            }
+            val response = GeocodingClient.instance.getGeocoding(address, GeocodingClient.API_KEY)
+            return response.results.firstOrNull()
         }
     }
 }
